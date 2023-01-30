275cacc40088499e0098ef826330cc6c4f92eb0a
Toryn <23torync@alice-smith.edu.my>
2023-01-30 22:51:21 +0800

    Update game.py

--- game.py
+++ game.py
@@ -2,20 +2,55 @@ import pygame,sys
 
 #pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1300,750))
+
+display = pygame.display.set_mode((1000,1000))
 clock = pygame.time.Clock()
+pygame.display.set_caption("Top Down Shooter")
 
+#player class
+class Player(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        super().__init__()
+        self.image = pygame.Surface([50,45])
+        self.image.fill((10,10,120))
+        self.rect = self.image.get_rect()
+        self.rect.y = y
+        self.rect.x = x
+        self.change_x = 0
+        self.change_y = 0
+
+#enemy class
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self,x,y,human):
+        super().__init__()
+        self.human = human
+        self.image = pygame.Surface([50,45])
+        self.image.fill((255,0,0))
+        self.rect = self.image.get_rect()
+        self.rect.y = y
+        self.rect.x = x
+
+        self.change_x = 0
+        self.change_y = 0
+
+spritelist = pygame.sprite.Group()
+player = Player(50,50)
+spritelist.add(player)
+enemy1 = Enemy(500,50,player)
+spritelist.add(enemy1)
+
+#game loop
 while True:
-    screen.fill((0,0,0))
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    spritelist.update()
+    spritelist.draw(display)
 
     clock.tick(60)
     pygame.display.update()
-    print('hsadfasdfasdf')
+    
 
 
 
